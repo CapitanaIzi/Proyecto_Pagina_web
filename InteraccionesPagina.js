911d99ec581b6dedbd21e385b0c8ebe59af5beb6
Francesca <guizarfrancesca7@gmail.com>
2024-11-14 18:51:53 -0300

    agrege logo y modifique el menu

--- InteraccionesPagina.js
+++ InteraccionesPagina.js
@@ -1,33 +1,8 @@
 // Clase para las interacciones de la página
 class InteraccionesPagina {
   constructor() {
-    this.configurarCambioMenu();
-    this.configurarClicFueraDelMenu();
     this.configurarCambioTema();
     this.aplicarTemaGuardado();
-  }
-
-  // Configura la interacción del menú
-  configurarCambioMenu() {
-    const botonMenu = document.querySelector('.menu-btn');
-    const menuPrincipal = document.querySelector('.menuPrincipal');
-
-    botonMenu.addEventListener('click', (e) => {
-      e.stopPropagation();
-      menuPrincipal.classList.toggle('active');
-    });
-  }
-
-  // Cierra el menú si se hace clic fuera de él
-  configurarClicFueraDelMenu() {
-    const botonMenu = document.querySelector('.menu-btn');
-    const menuPrincipal = document.querySelector('.menuPrincipal');
-
-    document.addEventListener('click', (e) => {
-      if (!botonMenu.contains(e.target) && !menuPrincipal.contains(e.target)) {
-        menuPrincipal.classList.remove('active');
-      }
-    });
   }
 
   // Configura el cambio de tema
@@ -39,7 +14,7 @@ class InteraccionesPagina {
       document.body.classList.toggle('dark-theme');
 
       const esTemaOscuro = document.body.classList.contains('dark-theme');
-      enlaceApariencia.textContent = esTemaOscuro ? 'Apariencia Clara' : 'Apariencia Oscura';
+      enlaceApariencia.textContent = esTemaOscuro ? 'Tema Claro' : 'Tema Oscuro';
 
       // Guardar la preferencia de tema en localStorage
       localStorage.setItem('theme', esTemaOscuro ? 'dark' : 'light');
@@ -53,9 +28,9 @@ class InteraccionesPagina {
 
     if (temaGuardado === 'dark') {
       document.body.classList.add('dark-theme');
-      enlaceApariencia.textContent = 'Apariencia Clara';
+      enlaceApariencia.textContent = 'Tema Claro';
     } else {
-      enlaceApariencia.textContent = 'Apariencia Oscura';
+      enlaceApariencia.textContent = 'Tema Oscuro';
     }
   }
 }
@@ -64,8 +39,6 @@ class InteraccionesPagina {
 
 // Crear instancias de las clases cuando la página se cargue
 document.addEventListener('DOMContentLoaded', () => {
-  // Instancia de la clase de Interacciones de la Página
-  const interaccionesPagina = new InteraccionesPagina();
 
   // Instancia del carrusel
   const carrusel = new Carrusel('.carousel-item');
